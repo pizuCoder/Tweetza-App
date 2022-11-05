ab7ef5916569aaf793e8118758f8083e03fff266
Priya Moghe <102720207+pizuCoder@users.noreply.github.com>
2022-11-05 16:47:23 +0530

    some changes

--- data.js
+++ data.js
@@ -4,8 +4,7 @@ export const tweetsData = [
         profilePic: `images/troll.jpg`,
         likes: 27,
         retweets: 10,
-        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
-            Guaranteed return on investment. HMU DMs open!!`,
+        tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. Guaranteed return on investment. HMU DMs open!!`,
         replies: [],
         isLiked: false,
         isRetweeted: false,
